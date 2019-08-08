71d4b1103fb96559ea5efa02d0a4ecf978395791
wuxii <wuxii@foxmail.com>
2019-08-08 17:44:33 +0800

    prepare security

--- src/main/java/com/harmony/cmb/core/controller/IndexController.java
+++ src/main/java/com/harmony/cmb/core/controller/IndexController.java
@@ -1,11 +1,24 @@
 package com.harmony.cmb.core.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.harmony.umbrella.web.Response;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 /**
  * @author wuxii
  */
 @RestController
 public class IndexController {
+
+    @PostMapping("/login")
+    public Response<String> login() {
+        return Response.ok(UUID.randomUUID().toString());
+    }
+
+    @RequestMapping(path = "/logout", method = {RequestMethod.POST, RequestMethod.GET, RequestMethod.DELETE})
+    public Response<String> logout() {
+        return Response.ok();
+    }
 
 }
