2708f3ad10a76dad2ad18c1609e248767fe85051
wuxii <wuxii@foxmail.com>
2019-10-16 14:56:55 +0800

    update r2dbc version

--- src/main/java/com/mindbook/word/domain/Sentence.java
+++ src/main/java/com/mindbook/word/domain/Sentence.java
@@ -3,12 +3,14 @@ package com.mindbook.word.domain;
 import com.mindbook.core.domain.AbstractPersistable;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import org.springframework.data.relational.core.mapping.Table;
 
 /**
  * @author wuxii
  */
+@EqualsAndHashCode(callSuper = true)
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
--- src/test/java/com/mindbook/CloudMindBookApplicationTests.java
+++ src/test/java/com/mindbook/CloudMindBookApplicationTests.java
@@ -1,16 +1,36 @@
 package com.mindbook;
 
+import com.mindbook.word.domain.Word;
+import io.r2dbc.spi.ConnectionFactory;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.r2dbc.core.DatabaseClient;
 import org.springframework.test.context.junit4.SpringRunner;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import java.time.Duration;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class CloudMindBookApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Autowired
+    private ConnectionFactory connectionFactory;
+
+    @Test
+    public void contextLoads() {
+        DatabaseClient client = DatabaseClient.create(connectionFactory);
+
+        Mono<Word> execResult = client.execute("select * from mindbook_word where id = $1")
+                .as(Word.class)
+                .bind(0, 1)
+                .fetch()
+                .one();
+
+        StepVerifier.create(execResult).expectComplete().verify(Duration.ofSeconds(10));
+    }
 
 }
